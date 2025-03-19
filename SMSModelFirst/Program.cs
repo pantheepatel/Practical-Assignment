24cdae16b0fad7e62c0cc0a49267a5838940932b
pantheepatel <patel.panthee.18@gmail.com>
2025-03-19 19:56:38 +0530

    CRUD Operations, problem in deleting with LINQ LastOrDefault

--- SMSModelFirst/Program.cs
+++ SMSModelFirst/Program.cs
@@ -1,15 +1,83 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMSModelFirst
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var context = new SMSModelDBContainer())
+            {
+                //// making 3 courses objects
+                //var course1 = new Course
+                //{
+                //    CourseName = ".NET",
+                //    CreditHours = 50
+                //};
+                //var course2 = new Course
+                //{
+                //    CourseName = "Java",
+                //    CreditHours = 30
+                //};
+                //var course3 = new Course
+                //{
+                //    CourseName = "Python",
+                //    CreditHours = 40
+                //};
+                //List<Course> courses = new List<Course> { course1, course2, course3 };
+                //context.Courses.AddRange(courses); // adding courses to the database
+
+                //// making 2 student objects
+                //var student1 = new Student
+                //{
+                //    StudentName = "Panthee Patel",
+                //    StudentAge = 20,
+                //    Courses = new List<Course> { course1, course3 },
+                //};
+                //var student2 = new Student
+                //{
+                //    StudentName = "Himanshi Gandhi",
+                //    StudentAge = 21,
+                //    Courses = new List<Course> { course1, course2 },
+                //};
+                //List<Student> students = new List<Student> { student1, student2 };
+                //context.Students.AddRange(students); // adding students to the database
+                //context.SaveChanges(); // saving changes to the database
+
+                //// getting student details from database
+                //var studentDetails = context.Students.ToList();
+                //foreach (var student in studentDetails)
+                //{
+                //    Console.WriteLine("Student Name: {0}", student.StudentName);
+                //    foreach (var course in student.Courses) // getting navigational property(Courses) of student
+                //    {
+                //        Console.WriteLine("\tCourse Name: {0}", course.CourseName);
+                //    }
+                //    Console.WriteLine();
+                //}
+
+                //// editing details of student
+                //var studentToEdit = context.Students.FirstOrDefault(s => s.StudentName == "Panthee Patel");
+                //context.Students.Attach(studentToEdit);
+
+                //if (studentToEdit != null)
+                //{
+                //    studentToEdit.StudentName = "Panthee Sharadbhai Patel";
+                //    //context.Entry(studentToEdit).State = System.Data.Entity.EntityState.Modified;
+                //    context.Entry(studentToEdit);
+                //    context.SaveChanges();
+                //}
+
+                // deleting student
+                var studentToDelete = context.Students.LastOrDefault(s => s.StudentName == "Panthee Sharadbhai Patel");
+                if (studentToDelete != null)
+                {
+                    context.Students.Remove(studentToDelete);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
---
+++ SMSModelFirst/SMSModelDB.edmx.sql
@@ -0,0 +1,105 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/19/2025 19:02:16
+-- Generated from EDMX file: C:\Users\panthee.patel\Desktop\submissions\practical-assignment\Assignment\SMSModelFirst\SMSModelDB.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [SMSModelFirstDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Students'
+CREATE TABLE [dbo].[Students] (
+    [StudentId] int IDENTITY(1,1) NOT NULL,
+    [StudentName] nvarchar(max)  NOT NULL,
+    [StudentAge] smallint  NOT NULL
+);
+GO
+
+-- Creating table 'Courses'
+CREATE TABLE [dbo].[Courses] (
+    [CourseId] int IDENTITY(1,1) NOT NULL,
+    [CourseName] nvarchar(max)  NOT NULL,
+    [CreditHours] smallint  NOT NULL
+);
+GO
+
+-- Creating table 'StudentCourse'
+CREATE TABLE [dbo].[StudentCourse] (
+    [Students_StudentId] int  NOT NULL,
+    [Courses_CourseId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [StudentId] in table 'Students'
+ALTER TABLE [dbo].[Students]
+ADD CONSTRAINT [PK_Students]
+    PRIMARY KEY CLUSTERED ([StudentId] ASC);
+GO
+
+-- Creating primary key on [CourseId] in table 'Courses'
+ALTER TABLE [dbo].[Courses]
+ADD CONSTRAINT [PK_Courses]
+    PRIMARY KEY CLUSTERED ([CourseId] ASC);
+GO
+
+-- Creating primary key on [Students_StudentId], [Courses_CourseId] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [PK_StudentCourse]
+    PRIMARY KEY CLUSTERED ([Students_StudentId], [Courses_CourseId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Students_StudentId] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [FK_StudentCourse_Student]
+    FOREIGN KEY ([Students_StudentId])
+    REFERENCES [dbo].[Students]
+        ([StudentId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Courses_CourseId] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [FK_StudentCourse_Course]
+    FOREIGN KEY ([Courses_CourseId])
+    REFERENCES [dbo].[Courses]
+        ([CourseId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_StudentCourse_Course'
+CREATE INDEX [IX_FK_StudentCourse_Course]
+ON [dbo].[StudentCourse]
+    ([Courses_CourseId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
